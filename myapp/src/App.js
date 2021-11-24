7da5cb990031deda02f93d673fb287a3a9d8f944
alfaroq <alfaroqalshreef@gmail.com>
2021-11-24 11:44:08 -0900

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import image1 from "./images/nasaa.png";
+function App() {
+  return (
+    <div className="App">
+<img src={image1}/>
+<img src="/images/welcome.jpg" className="hello" />
+    </div>
+  );
+}
+
+export default App;
